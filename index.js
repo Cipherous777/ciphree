f464617a5c51159cc3c666b75b21e1d51936cee5
Cipherous <elan.mpr@gmail.com>
2026-01-26 08:50:34 -0800

    Initial commit without secrets

---
+++ index.js
@@ -0,0 +1,53 @@
+// requiring modules
+const express = require("express");
+const app = express()
+const PORT = 8888
+const admin = require("firebase-admin")
+const serviceAccount = require('./firebase-key.json'); 
+const { credential } = require("firebase-admin");
+
+// middleware
+app.set("view engine" , "ejs")
+app.use(express.json())
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+
+
+//firestore commands
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+const db = admin.firestore()
+const auth = admin.auth()
+// get and post commands
+app.get("/" , (req,res)=>{
+res.render("landing.ejs")
+})
+app.get("/earlyAccess" , (req,res)=>{
+    res.render("earlyAccess.ejs")
+})
+ app.post("/earlyAccess", (req,res)=>{
+    try{
+     const {name,email} = req.body
+     
+     const storeData = db.collection("earlyAccess").add({
+        name:name,
+        email:email,
+        date: new Date()
+     })
+      res.redirect("/")
+    }catch(err){
+     console.log(err)
+     res.redirect("/earlyAccess")
+    }
+ })
+
+
+ 
+
+
+// listening to the port
+app.listen(PORT,()=>{
+    console.log(`Server is running at ${PORT}`)
+})
